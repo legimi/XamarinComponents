98795dd192dc791fa31a1a977dffda57969995af
Paweł Kanarek <pawel.kanarek@legimi.pl>
2023-05-04 17:04:35 +0200

    update .net 7

--- iOS/MaterialComponents/nuget/g_nuspec.ps1
+++ iOS/MaterialComponents/nuget/g_nuspec.ps1
@@ -1,4 +1,4 @@
-$dir = "/Users/pawel.kanarek/Git/XamarinComponents/iOS/MaterialComponents/source/MaterialComponents/bin/Release/net6.0-ios/MaterialComponents.resources/"
+$dir = "/Users/pawel.kanarek/Git/XamarinComponents/iOS/MaterialComponents/source/MaterialComponents/bin/Release/net7.0-ios/MaterialComponents.resources/"
 $files = Get-ChildItem $dir -Recurse
 
 foreach ($file in $files) {
@@ -6,13 +6,13 @@ foreach ($file in $files) {
   $targetPath = ""
   if ($relativePath.EndsWith("/Info.plist")) {
     $path = $relativePath.Replace("/Info.plist", "")
-    $targetPath = "lib/net6.0-ios16.1/MaterialComponents.resources/$path"
+    $targetPath = "lib/net7.0-ios16.2/MaterialComponents.resources/$path"
   }
   else {
     $pathWithoutFileName = $relativePath.Replace($file.Name, "")
     $path = $pathWithoutFileName.Substring(0, $path.Length - 1)
-    $targetPath = "lib/net6.0-ios16.1/MaterialComponents.resources/$path"
+    $targetPath = "lib/net7.0-ios16.2/MaterialComponents.resources/$path"
   }
-  $output = "<file src=`"../source/MaterialComponents/bin/Release/net6.0-ios/MaterialComponents.resources/$relativePath`" target=`"$targetPath`" />"
+  $output = "<file src=`"../source/MaterialComponents/bin/Release/net7.0-ios/MaterialComponents.resources/$relativePath`" target=`"$targetPath`" />"
   Write-Output $output
 }
